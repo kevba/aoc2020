ae9a8df80935c427019264c85bdfb64d2ea96219
Kevin Batema <kevinbatema@gmail.com>
2020-12-07 23:28:34 +0100

    Day 7 part 1 and part 2

---
+++ day7/part1/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+	defer log.Print(time.Since(startTime))
+
+	input, _ := getInput()
+	bagMap := inputToBagMap(input)
+
+	answer := solve(bagMap)
+
+	log.Printf("anwser: %v", answer)
+}
+
+func solve(bags map[string]bag) int {
+	count := countContainingBags("shiny gold bag", bags)
+
+	// The golden bag does not count of course.
+	return count - 1
+}
+
+func countContainingBags(bagName string, bags map[string]bag) int {
+	// Each bag "contains" itself
+	containedSum := 1
+
+	for containedName, containedCount := range bags[bagName].contains {
+		amount := countContainingBags(containedName, bags) * containedCount
+		containedSum = containedSum + amount
+	}
+
+	return containedSum
+}
+
+type bag struct {
+	name        string
+	containedBy []string
+	contains    map[string]int
+}
+
+func inputToBagMap(input []string) map[string]bag {
+	bagMap := map[string]bag{}
+
+	for _, inLine := range input {
+		newBag := lineToBag(inLine)
+
+		if existingBag, ok := bagMap[newBag.name]; ok {
+			newBag.containedBy = existingBag.containedBy
+		}
+
+		bagMap[newBag.name] = newBag
+
+		for containedName := range newBag.contains {
+			containedBag, ok := bagMap[containedName]
+
+			if !ok {
+				containedBag = bag{
+					name:        containedName,
+					containedBy: []string{},
+					contains:    map[string]int{},
+				}
+			}
+
+			containedBag.containedBy = append(containedBag.containedBy, newBag.name)
+			bagMap[containedName] = containedBag
+		}
+	}
+
+	return bagMap
+}
+
+var lineRegex = regexp.MustCompile("([a-z]* [a-z]* [a-z]*) contain(.*)")
+
+func lineToBag(in string) bag {
+	newBag := bag{
+		name:        "",
+		containedBy: []string{},
+		contains:    map[string]int{},
+	}
+
+	match := lineRegex.FindStringSubmatch(in)
+
+	name := match[1]
+	name = name[:len(name)-1]
+	newBag.name = name
+
+	for _, containBag := range strings.Split(match[2], ",") {
+		if containBag == " no other bags." {
+			continue
+		}
+
+		containBagAmount := atoi(containBag[1:2])
+		containBagName := containBag[3:]
+
+		if containBagName[len(containBagName)-1] == '.' {
+			containBagName = containBagName[:len(containBagName)-1]
+		}
+
+		if containBagAmount > 1 {
+			containBagName = containBagName[:len(containBagName)-1]
+		}
+
+		newBag.contains[containBagName] = containBagAmount
+	}
+
+	return newBag
+}
+
+var inputFile = "../input.txt"
+
+func getInput() ([]string, error) {
+	values := []string{}
+	file, err := os.Open(inputFile)
+
+	if err != nil {
+		return values, err
+	}
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		line := s.Text()
+		values = append(values, line)
+	}
+
+	return values, nil
+}
+
+func atoi(s string) int {
+	num, _ := strconv.Atoi(s)
+	return num
+}
